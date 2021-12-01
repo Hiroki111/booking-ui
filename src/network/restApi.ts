937890cc5b8f2daeabd15eddd8102671c66fc2e1
Hiroki Takahashi <git@hiroki-t.com>
2021-12-01 09:18:10 +0200

    Updated REST API functions with mock data

--- src/network/restApi.ts
+++ src/network/restApi.ts
@@ -1,81 +1,23 @@
-import axios, { AxiosResponse } from 'axios';
-
-import { AuthenticateResponseBodyDto } from '../interfaces/authenticate';
 import { StaffDto } from '../interfaces/staff';
 import { ServiceTypeDto } from '../interfaces/serviceType';
 import { BookingRequestDto } from '../interfaces/booking';
-
-const defaultHeaders = {
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
-};
+import { mockServiceTypes, mockStaffLinst, mockCaptcha } from './mockData';
 
 const restApi = {
-  authenticate: function (email: string, password: string): Promise<AxiosResponse<AuthenticateResponseBodyDto>> {
-    return axios({
-      method: 'POST',
-      url: '/api/auth/login',
-      data: { email, password },
-      headers: defaultHeaders,
-    });
-  },
-
-  logout: function (): Promise<AxiosResponse<string>> {
-    return axios({
-      method: 'POST',
-      url: '/api/auth/logout',
-      headers: defaultHeaders,
-    });
-  },
-
-  checkLoginStatus: function (): Promise<AxiosResponse<string>> {
-    return axios({
-      method: 'GET',
-      url: '/api/auth/checkToken',
-    });
-  },
-
   fetchServiceTypes: async function (): Promise<ServiceTypeDto[]> {
-    const res: AxiosResponse<ServiceTypeDto[]> = await axios({
-      method: 'GET',
-      url: '/api/serviceTypes',
-    });
-    return res.data;
+    return new Promise((resolve) => setTimeout(() => resolve(mockServiceTypes), 500));
   },
 
   fetchStaffList: async function (): Promise<StaffDto[]> {
-    const res: AxiosResponse<StaffDto[]> = await axios({
-      method: 'GET',
-      url: '/api/staff',
-    });
-    return res.data;
+    return new Promise((resolve) => setTimeout(() => resolve(mockStaffLinst), 500));
   },
 
   fetchCaptcha: async function (): Promise<string> {
-    const res: AxiosResponse<string> = await axios({
-      method: 'GET',
-      url: '/api/captcha',
-      headers: defaultHeaders,
-    });
-    return res.data;
+    return new Promise((resolve) => setTimeout(() => resolve(mockCaptcha), 500));
   },
 
-  bookAppointment: async function (data: BookingRequestDto): Promise<void> {
-    // TODO: Make a custom util for calling REST APIs, so that
-    // the catch block like the one below won't be necessary
-    try {
-      await axios({
-        method: 'POST',
-        url: '/api/booking',
-        data: data,
-        headers: defaultHeaders,
-      });
-    } catch (error: any) {
-      if (error.isAxiosError) {
-        throw new Error(error.response.data.message);
-      }
-      throw new Error(error);
-    }
+  bookAppointment: async function (_data: BookingRequestDto): Promise<void> {
+    return new Promise((resolve) => setTimeout(() => resolve(), 500));
   },
 };
 
